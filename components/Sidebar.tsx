41dcbaa2ec5c1c82168575919344805b91cbf0fe
Erick Briones <erickalan518@gmail.com>
2023-07-31 18:20:56 -0600

    Dashboard page

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Montserrat } from 'next/font/google';
+import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -65,6 +66,7 @@ const routes = [
 ];
 
 const Sidebar = () => {
+	const pathname = usePathname();
 	return (
 		<div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
 			<div className="px-3 py-2 flex-1">
@@ -94,7 +96,12 @@ const Sidebar = () => {
 						<Link
 							key={route.href}
 							href={route.href}
-							className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+							className={cn(
+								'text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition',
+								pathname === route.href
+									? 'text-white bg-white/10'
+									: 'text-zinc-400'
+							)}
 						>
 							<div className="flex items-center flex-1">
 								<route.icon
